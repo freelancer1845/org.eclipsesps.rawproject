4e978388ca027bdcc9b76d8b5dac27c54daa4e6d
csat8630 <JaschaRiedel@student.uibk.ac.at>
2017-12-11 12:04:55 +0100

    Added Linux gtk to target. Fixed classpaths problems

--- org.eclipsesps/bundles/org.eclipsesps.ui/src/org/eclipsesps/ui/edit/DigitalCheckBoxField.java
+++ org.eclipsesps/bundles/org.eclipsesps.ui/src/org/eclipsesps/ui/edit/DigitalCheckBoxField.java
@@ -1,12 +1,18 @@
 package org.eclipsesps.ui.edit;
 
-import java.awt.Button;
-import java.awt.GridLayout;
-import java.awt.Label;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipsesps.spsmodel.AbstractIOField;
 import org.eclipsesps.spsmodel.AbstractWriteableIOField;
 import org.eclipsesps.ui.utils.AbstractDigitalBindingEContentAdapter;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DigitalCheckBoxField extends Composite {
@@ -18,20 +24,11 @@ public class DigitalCheckBoxField extends Composite {
 
   private final AbstractIOField<Boolean> digitalField;
 
-  private final Label label;
+  private Label label;
 
-  private final Button cbButton;
+  private Button cbButton;
 
-  private final boolean readOnly;
-
-  public DigitalCheckBoxField(Composite parent, int style, String labelText,
-      AbstractIOField<Boolean> digitalField) {
-    boolean pReadOnly = false;
-    if ((digitalField instanceof AbstractWriteableIOField<?>) == false) {
-      pReadOnly = true;
-    }
-    DigitalEditField(parent, style, labelText, digitalField, pReadOnly);
-  }
+  private boolean readOnly;
 
   public DigitalCheckBoxField(Composite parent, int style, String labelText,
       AbstractIOField<Boolean> digitalField, boolean readOnly) {
@@ -46,6 +43,15 @@ public class DigitalCheckBoxField extends Composite {
     addListeners();
   }
 
+  public DigitalCheckBoxField(Composite parent, int style, String labelText,
+      AbstractIOField<Boolean> digitalField) {
+    this(parent, style, labelText, digitalField, false);
+    if ((digitalField instanceof AbstractWriteableIOField<?>) == false) {
+      readOnly = true;
+    }
+  }
+
+
   private void createLayout() {
     GridLayout layout = new GridLayout();
     layout.numColumns = 2;
@@ -56,7 +62,7 @@ public class DigitalCheckBoxField extends Composite {
 
   private void createControls() {
     label = new Label(this, SWT.NONE);
-    label.setText(labeltext);
+    label.setText(labelText);
     label.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false));
     cbButton = new Button(this, SWT.PUSH);
     cbButton.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
@@ -76,11 +82,11 @@ public class DigitalCheckBoxField extends Composite {
 
     });
 
-    digitalOutput.eAdapters().add(new AbstractDigitalBindingEContentAdapter() {
+    digitalField.eAdapters().add(new AbstractDigitalBindingEContentAdapter() {
 
       @Override
       protected void stateChanged(boolean newBooleanValue) {
-        Display.getDefault().asyncExec(() -> cbButton.setSelection(newBooelanvalue));
+        Display.getDefault().asyncExec(() -> cbButton.setSelection(newBooleanValue));
       }
     });
   }
